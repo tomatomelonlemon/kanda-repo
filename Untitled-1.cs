e6ebc43b3d1f86c893da61bba469bc6cde86c865
tomatomelonlemon <Kanda.Yoshiki@technopro.com>
2023-06-07 15:49:34 +0900

    Hello

---
+++ Untitled-1.cs
@@ -0,0 +1,12 @@
+using System;
+
+    namespace Untitle
+    {
+        class Untitle
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("あ");
+            }
+        }
+    }    
